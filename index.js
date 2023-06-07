3b1bee171fe45410c24f4dbd0dd906d64eaad522
robiul-romjan <robiulromjan7@gmail.com>
2023-06-08 01:06:22 +0600

    admin work done

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhjeumo.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,6 +49,30 @@ async function run() {
           return res.send({message: "User already exists"})
         }
         const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
+    //   make user admin
+    app.patch("/users/admin/:id", async(req, res)=> {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updateDoc = {
+          $set: {
+            role: "admin"
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      });
+    //   make user admin
+    app.patch("/users/instructor/:id", async(req, res)=> {
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)};
+        const updateDoc = {
+          $set: {
+            role: "instructor"
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
         res.send(result);
       });
 
